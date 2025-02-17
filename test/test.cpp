587cfddf665247262f1ca99d4c46e4d01b25368f
Vanzha Kirill <vanzha.kirill@gmail.com>
2025-02-17 20:29:17 +0300

    Add tests for default constructor

--- test/test.cpp
+++ test/test.cpp
@@ -3,3 +3,18 @@
 
 #define LONGNUM_TEST_PRIVATE
 #include "longnum.hpp"
+
+using namespace ln;
+
+TEST_CASE("Default constructor") {
+    Longnum num{};
+
+    CHECK(num.sign() == 0);
+    num.flip_sign();
+    CHECK(num.sign() == 0);
+
+    CHECK(num.get_precision() == 0);
+    CHECK(num.to_string(0) == "0");
+    CHECK(num.to_string(5) == "0.00000");
+    CHECK(num.digits.size() == 0);
+}
